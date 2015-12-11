8a2e92d15b082b91022c0490e00f34e67a2dafba
mmotkina <mariamotkina@yandex.ru>
2015-12-11 19:12:38 +0300

    Test if nested suites display in right order

---
+++ test/nesting/nesting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Check nested tests displays right - parent', function () {
+    describe('Check nested tests displays right - child', function () {
+        it('should be shild', function (done) {
+            done();
+        });
+    });
+});
---
+++ test/nesting/nesting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OUTPUT=$(bin/mocha-parallel-tests -R spec --timeout 60000 --slow 30000 test/nesting/nesting.js 2>&1)
+strindex() { 
+  x="${1%%$2*}"
+  [[ $x = $1 ]] && echo -1 || echo ${#x}
+}
+PARENT=$(strindex "$OUTPUT" "parent")
+CHILD=$(strindex "$OUTPUT" "child")
+echo "${PARENT}"
+echo "${CHILD}"
+if [[ PARENT -lt CHILD ]]
+then
+    exit 0;
+    else
+    exit 1;
+fi
