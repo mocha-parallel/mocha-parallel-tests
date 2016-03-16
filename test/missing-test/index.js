bb99b2bebcf496e22748cd22e2561c5987124c36
Dmitry Sorin <dsorin@yandex-team.ru>
2016-03-16 15:14:18 +0300

    fix missing file test

--- test/missing-test/index.js
+++ test/missing-test/index.js
@@ -4,11 +4,10 @@ var assert = require('assert');
 var path = require('path');
 var exec = require('child_process').exec;
 var libExecutable = path.resolve(__dirname, '../../bin/mocha-parallel-tests');
-var libExecutable = path.resolve(__dirname, '../../node_modules/.bin/mocha');
 
 exec(libExecutable + ' -R doc --timeout 60000 --slow 30000 test/missing-test/missing.js', {
     cwd: path.resolve(__dirname, '../../')
 }, function (err) {
     assert(err, 'Err should exist');
-    assert.strictEqual(err.code, 1, 'Error code should equal 1');
+    assert.strictEqual(err.code, 8, 'Error code should equal 8');
 });
