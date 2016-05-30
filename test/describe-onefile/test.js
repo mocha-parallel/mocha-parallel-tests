f4469e99a44700213f519dcc75e29bd20c1da68f
Dmitry Sorin <dsorin@yandex-team.ru>
2016-05-30 20:04:42 +0300

    Add failing test for failures

--- test/describe-onefile/test.js
+++ test/describe-onefile/test.js
@@ -3,7 +3,11 @@
 for (let i = 0; i < 3; i++) {
     describe(`suite #${i}`, () => {
         it('should run its case', done => {
-            setTimeout(done, 1000);
+            if (i === 0) {
+                setTimeout(done, 1000, new Error('oops'));
+            } else {
+                setTimeout(done, 1000);
+            }
         });
     });
 }
