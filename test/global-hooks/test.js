53c4c5aa1ace89cbc36de22b7283d724487527d2
Dmitry Sorin <dsorin@yandex-team.ru>
2016-06-24 13:46:16 +0300

    Add simple global hooks test

---
+++ test/global-hooks/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+before(() => {
+    console.log('run global before hook');
+    throw new Error('foo');
+});
+
+after(() => {
+    console.log('run global after hook');
+    throw new Error('bar');
+});
+
+describe('Test suite', () => {
+    it('should end immediately', () => {});
+});
