87f6b60bb034f2fe730024a0e003e8a20c696c3e
Dmitry Sorin <info@staypositive.ru>
2016-07-18 18:14:02 +0300

    Support mocha.opts file (#45)
    * Support mocha.opts, fixes #44
    
    * Use more descriptive logs for mocha.opts test

---
+++ test/mocha-opts/index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TIMESTAMP_START="$(date +%s)"
+
+OUTPUT=$(dist/bin/mocha-parallel-tests -R spec test/mocha-opts/tests --slow 10000 --opts test/mocha-opts/mocha.opts)
+STATUS=$?
+TIMESTAMP_FINISH="$(date +%s)"
+TIMESTAMP_DIFF=`expr $TIMESTAMP_FINISH - $TIMESTAMP_START`
+
+if [ $STATUS -eq 0 ]; then
+    if [[ $TIMESTAMP_DIFF -lt 10 ]]; then
+        exit 0
+    else
+        echo "Running time is too big: $TIMESTAMP_DIFF"
+        exit 1
+    fi
+else
+    echo "Exit code is wrong: $STATUS"
+    exit 1
+fi
