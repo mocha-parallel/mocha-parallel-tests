a9aedec84d37ed3e7e6f4954277a6067e1ed412b
Dmitry Sorin <dsorin@yandex-team.ru>
2016-07-18 18:06:28 +0300

    Use more descriptive logs for mocha.opts test

--- test/mocha-opts/index.sh
+++ test/mocha-opts/index.sh
@@ -3,13 +3,18 @@
 TIMESTAMP_START="$(date +%s)"
 
 OUTPUT=$(dist/bin/mocha-parallel-tests -R spec test/mocha-opts/tests --slow 10000 --opts test/mocha-opts/mocha.opts)
-
+STATUS=$?
 TIMESTAMP_FINISH="$(date +%s)"
 TIMESTAMP_DIFF=`expr $TIMESTAMP_FINISH - $TIMESTAMP_START`
-echo "Tests running time was $TIMESTAMP_DIFF seconds"
 
-if [[ $TIMESTAMP_DIFF -lt 10 ]]; then
-    exit 0
+if [ $STATUS -eq 0 ]; then
+    if [[ $TIMESTAMP_DIFF -lt 10 ]]; then
+        exit 0
+    else
+        echo "Running time is too big: $TIMESTAMP_DIFF"
+        exit 1
+    fi
 else
+    echo "Exit code is wrong: $STATUS"
     exit 1
 fi
