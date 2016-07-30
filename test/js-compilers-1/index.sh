a1aa1558e0f7750deaaef119cb5e6b2044fdcd0b
Dmitry Sorin <dsorin@yandex-team.ru>
2016-07-30 22:03:09 +0300

    Add failing test for js:file --compilers

---
+++ test/js-compilers-1/index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT=$(dist/bin/mocha-parallel-tests --compilers js:test/js-compilers-1/babel-register.js test/js-compilers-1/test.js 2>&1)
+STATUS=$?
+
+if [ $STATUS -eq 0 ]; then
+    exit 0
+else
+    echo "Exit code wrong: $STATUS"
+    echo "Output: $OUTPUT"
+
+    exit 1
+fi
