57e250b46f2b7d8addb1134e0fb7df7bacbc817a
Dmitry Sorin <dsorin@yandex-team.ru>
2016-07-30 18:32:01 +0300

    Fix lint errors

--- test/require-option/require-module.js
+++ test/require-option/require-module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-global.foo = () => {}
+global.foo = () => {};
--- test/require-option/test.js
+++ test/require-option/test.js
@@ -2,6 +2,8 @@
 
 describe('Test suite', function () {
     it('should not throw', function () {
+        /* eslint-disable no-undef */
         foo();
+        /* eslint-enable no-undef */
     });
 });
