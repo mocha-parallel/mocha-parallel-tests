1366f5411ce9ad4738f000de08137dd95da2e127
Dmitry Sorin <info@staypositive.ru>
2016-10-23 07:10:42 -0700

    Support tap reporter (#79)
    * Add test for failing tap reporter, relates #78
    * Refactor move helpers and libs into lib/utils
    * Use mocha-extended runner
    * Add debug info to hooks
    * Use default mocha behaviour for mocha runner

---
+++ test/reporter-native-tap/index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT=$(dist/bin/mocha-parallel-tests -R tap test/reporter-native-tap/suite.js 2>&1)
+STATUS=$?
+
+if [ $STATUS -eq 0 ]; then
+    exit 0
+else
+    echo "Exit code is unexpected: $STATUS"
+    echo "Debug output: $OUTPUT"
+
+    exit 1
+fi
