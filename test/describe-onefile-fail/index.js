ea33aa8fa462e321074ba5de45e80c522b60058a
Dmitry Sorin <dsorin@yandex-team.ru>
2016-12-05 17:33:16 +0300

    Fix test

--- test/describe-onefile-fail/index.js
+++ test/describe-onefile-fail/index.js
@@ -5,16 +5,16 @@ const path = require('path');
 const exec = require('child_process').exec;
 const libExecutable = path.resolve(__dirname, '../../dist/bin/mocha-parallel-tests');
 
-exec(`${libExecutable} -R json test/describe-onefile-fail/test.spec.js`, {
+exec(`${libExecutable} -R json --retry 1 test/describe-onefile-fail/test.spec.js`, {
     cwd: path.resolve(__dirname, '../../')
 }, (err, stdout) => {
-    assert(!err, `Error occured: ${err.message}`);
+    assert(!err, `Error occured: ${err}`);
 
     let jsonReporterOutput = stdout.toString();
     try {
         jsonReporterOutput = JSON.parse(jsonReporterOutput);
     } catch (ex) {
-        console.error(`Native JSON reporter output is not valid JSON: ${jsonReporterOutput}`);
+        console.error(`Native JSON reporter output is not valid JSON: ${jsonReporterOutput || '(empty)'}`);
         process.exit(1);
     }
 
