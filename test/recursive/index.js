5a16541367bca9c5a73455e976431bc7de0de19c
Dmitry Sorin <info@staypositive.ru>
2017-01-28 10:15:48 +1100

    Add test for recursive support

---
+++ test/recursive/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const path = require('path');
+const exec = require('child_process').exec;
+const libExecutable = path.resolve(__dirname, '../../dist/bin/mocha-parallel-tests');
+
+exec(`${libExecutable} --recursive -R json test/recursive/tests/`, {
+    cwd: path.resolve(__dirname, '../../')
+}, (err, stdout) => {
+    assert(!err, `Error occured: ${err}`);
+
+    let jsonReporterOutput = stdout.toString();
+    try {
+        jsonReporterOutput = JSON.parse(jsonReporterOutput);
+    } catch (ex) {
+        console.error(`Native JSON reporter output is not valid JSON: ${jsonReporterOutput || '(empty)'}`);
+        process.exit(1);
+    }
+
+    assert.strictEqual(jsonReporterOutput.stats.suites, 2);
+    assert.strictEqual(jsonReporterOutput.stats.tests, 2);
+    assert.strictEqual(jsonReporterOutput.stats.passes, 2);
+    assert.strictEqual(jsonReporterOutput.stats.failures, 0);
+});
---
+++ test/recursive/tests/2/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('Test suite #2', () => {
+    it('test case 2', () => {});
+});
