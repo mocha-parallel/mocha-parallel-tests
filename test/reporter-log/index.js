682e9de1834604dab01c17478fee29e6f29788fd
k03mad <k03.mad@gmail.com>
2018-05-28 02:20:44 +0300

    typo fix

--- test/reporter-log/index.js
+++ test/reporter-log/index.js
@@ -8,15 +8,15 @@ const timings = new Set();
 mocha
     // the first test will be passed after few milliseconds
     .addFile(`${__dirname}/tests/log1.js`)
-    // the second test will be passed after three seconds
+    // the second test will be passed after 3 seconds
     .addFile(`${__dirname}/tests/log2.js`)
     .timeout(5000)
     .run()
-    // current seconds in suite ended time of each test should be not the same
+    // suite end time of each test should be not the same
     .on('suite end', () => timings.add((new Date()).getSeconds()))
     .on('end', () => {
         if (timings.size === 1) {
-            console.error('Reporter prints tests log after all tests are finished');
+            console.error('Reporter prints tests log after all tests are finished, instead of after each test');
             process.exit(1);
         }
     });
