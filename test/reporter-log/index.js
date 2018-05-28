504fd4ca5cc365e68f25c2463790b1439ae52c98
k03mad <k03.mad@gmail.com>
2018-05-28 17:25:02 +0300

    check time between events in test

--- test/reporter-log/index.js
+++ test/reporter-log/index.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
 const MochaParallelTests = require('../../dist/main/mocha').default;
-const mocha = new MochaParallelTests;
+const SilentReporter = require('../util/silent-reporter');
 
-const timings = new Set();
+const mocha = new MochaParallelTests;
+const timings = [];
 
 mocha
     // the first test will be passed after few milliseconds
     .addFile(`${__dirname}/tests/log1.js`)
     // the second test will be passed after 3 seconds
     .addFile(`${__dirname}/tests/log2.js`)
-    .timeout(5000)
+    .timeout(3000)
+    .reporter(SilentReporter)
     .run()
     // suite end time of each test should be not the same
-    .on('suite end', () => timings.add((new Date()).getSeconds()))
+    .on('suite end', () => timings.push((new Date()).getSeconds()))
     .on('end', () => {
-        if (timings.size === 1) {
-            console.error('Reporter prints tests log after all tests are finished, instead of after each test');
+        if (timings.pop() - timings[0] !== 2) {
+            console.error('Should have 2s between 2 test suite end events');
             process.exit(1);
         }
     });
--- test/reporter-log/tests/log2.js
+++ test/reporter-log/tests/log2.js
@@ -1,7 +1,7 @@
 describe('suite2', () => {
     it('case2', () => {
         return new Promise(resolve => {
-            setTimeout(resolve, 3000);
+            setTimeout(resolve, 2000);
         });
     });
 });
