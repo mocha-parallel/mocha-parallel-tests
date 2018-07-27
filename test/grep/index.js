5c2ff08d19027784a37c6b51acd7ef4985ad9eca
uafinder <hennadii.bulakh@sixt.com>
2018-07-27 12:44:07 +0200

    - add support option grep - add test

---
+++ test/grep/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var assert = require('assert');
+var path = require('path');
+var exec = require('child_process').exec;
+var libExecutable = path.resolve(__dirname, '../../dist/bin/cli.js');
+
+exec(libExecutable + ' -R json --grep grep test/grep/tests', {
+    cwd: path.resolve(__dirname, '../../')
+}, function (err, stderr) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    var jsonReporterOutput = stderr.toString();
+
+    try {
+        jsonReporterOutput = JSON.parse(jsonReporterOutput);
+    } catch (ex) {
+        console.error('Native JSON reporter output is not valid JSON: ' + jsonReporterOutput);
+        process.exit(1);
+    }
+
+    assert.strictEqual(jsonReporterOutput.stats.passes, 3, 'Tests number is wrong. Expected 3. Actual: '
+      + jsonReporterOutput.stats.passes);
+});
---
+++ test/grep/tests/test1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('tests file 2', () => {
+    it('should be run 3. grep', () => {
+        assert.ok(true);
+    });
+
+    it('should be skipped', () => {
+        assert.ok(true);
+    });
+});
+
