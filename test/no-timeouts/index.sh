cb17f958b7a9b12375f69ec671783a2648d6b52c
Dmitrii Sorin <dsorin@atlassian.com>
2019-04-16 08:18:37 +1000

    Improve no-timeouts test code

--- test/no-timeouts/index.sh
+++ test/no-timeouts/index.sh
@@ -8,9 +8,12 @@ MPT_STATUS_CODE_NO_TIMEOUT=$?
 
 # currently mocha exits with 2 if 2 tests failed, 3 if 3 tests failed etc
 # though it's a strange behaviour, mocha-parallel-tests behaviour should be the same
-if [ $MPT_STATUS_CODE -eq 2 ] && [ $MPT_STATUS_CODE_NO_TIMEOUT -eq 0 ]; then
-    exit 0
-else
-    echo "The flag no-timeouts did not work as expected: $MPT_STATUS_CODE (no flag) instead of 2 and $MPT_STATUS_CODE_NO_TIMEOUT (flag) instead of 0"
-    exit 1
+if [ $MPT_STATUS_CODE -ne 2 ]; then
+  echo "The flag no-timeouts did not work as expected: $MPT_STATUS_CODE (no flag) instead of 2"
+  exit 1
+fi
+
+if [ $MPT_STATUS_CODE_NO_TIMEOUT -ne 0 ]; then
+  echo "The flag no-timeouts did not work as expected: $MPT_STATUS_CODE_NO_TIMEOUT (flag) instead of 0"
+  exit 1
 fi
