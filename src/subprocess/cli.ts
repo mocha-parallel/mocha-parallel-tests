97a2f5246217d4ca57bae293660ad7c85e3732cf
Dmitrii Sorin <dsorin@atlassian.com>
2019-06-03 17:06:06 +1000

    Introduce intermediate CLI interface to consume process options

---
+++ src/subprocess/cli.ts
@@ -0,0 +1,74 @@
+import * as yargs from 'yargs';
+
+import { DEBUG_SUBPROCESS } from '../config';
+import { runMocha } from './runner';
+import { ThreadOptions } from '../thread';
+
+interface Args {
+  bail?: boolean;
+  compilers: string[];
+  delay?: boolean;
+  grep?: string;
+  enableTimeouts?: boolean;
+  exit?: boolean;
+  ['full-trace']?: boolean;
+  require: string[];
+  retries?: number;
+  slow?: boolean;
+  timeout?: number;
+  test: string;
+}
+
+function isDebugSubprocesss(argv: Args) {
+  return argv[DEBUG_SUBPROCESS.yargs] as boolean;
+}
+
+function threadOptionsFromArgv(argv: Args): ThreadOptions {
+  return {
+    bail: argv.bail,
+    compilers: argv.compilers,
+    delay: argv.delay || false,
+    enableTimeouts: argv.enableTimeouts,
+    exitImmediately: argv.exit || false,
+    fullTrace: argv['full-trace'] || false,
+    grep: argv.grep,
+    isTypescriptRunMode: isDebugSubprocesss(argv),
+    requires: argv.require,
+    retries: argv.retries,
+    slow: argv.slow,
+    timeout: argv.timeout,
+  };
+}
+
+const argv: Args = yargs
+  .boolean('bail')
+  .option('compilers', {
+    array: true,
+    default: [],
+  })
+  .boolean('delay')
+  .string('grep')
+  .boolean('enableTimeouts')
+  .option('exit', {
+    boolean: true,
+  })
+  .option('full-trace', {
+    boolean: true,
+  })
+  .boolean('slow')
+  .option('test', {
+    demandOption: true,
+    string: true,
+  })
+  .option('require', {
+    array: true,
+    default: [],
+  })
+  .number('retries')
+  .number('timeout')
+  .parse(process.argv);
+
+const debugSubprocess = isDebugSubprocesss(argv);
+const options = threadOptionsFromArgv(argv);
+
+runMocha(argv.test, options, debugSubprocess);
