66301944a82372530e36c03909a765887bfc8580
Dmitrii Sorin <dsorin@atlassian.com>
2019-06-11 08:04:21 +1000

    Do not check line breaks with eslint - editorconfig is enough

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -14,10 +14,6 @@ module.exports = {
             "error",
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
         "quotes": [
             "error",
             "single"
