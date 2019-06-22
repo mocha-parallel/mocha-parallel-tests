026ba8ad205b2e5cef7e5e173d41954ae40456e0
Dmitrii Sorin <dsorin@atlassian.com>
2019-06-22 22:23:52 +1000

    Fix tty issue

--- src/subprocess/thread/worker.development.js
+++ src/subprocess/thread/worker.development.js
@@ -2,10 +2,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { register: registerTypescriptHandler } = require('ts-node');
 
-// monkeypatch Node.JS native TTY function
-// otherwise mocha native base reporter throws exception
-// inside a worker environment
-require('tty').getWindowSize = () => 75;
-
 registerTypescriptHandler();
 require(`${__dirname}/worker.ts`);
--- src/subprocess/thread/worker.ts
+++ src/subprocess/thread/worker.ts
@@ -1,5 +1,9 @@
-import { workerData } from 'worker_threads';
+// monkeypatch Node.JS native TTY function
+// otherwise mocha native base reporter throws exception
+// inside a worker environment
+require('tty').getWindowSize = () => 75;
 
+import { workerData } from 'worker_threads';
 import { runMocha } from '../runner';
 import { WorkerData } from '../../main/thread/worker';
 
