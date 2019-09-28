44061a1091cc127f4d6867ee30b070d277d3f9b0
Jeroen Claassens <j.claassens@cgi.com>
2019-09-28 08:26:20 +0200

    test: fix the tests for the --file option
    fixes #222
    
    Signed-off-by: Jeroen Claassens <j.claassens@cgi.com>

--- test/file/index.js
+++ test/file/index.js
@@ -7,7 +7,6 @@ const { resolve } = require('path');
 const { exec } = require('child_process');
 const libExecutable = resolve(__dirname, '../../dist/bin/cli.js');
 
-
 exec(`${libExecutable} --file test/file/config.js -R test/file/reporter.js test/file/index.spec.js`, {
     cwd: resolve(__dirname, '../../'),
 }, (err, stdout) => {
@@ -16,19 +15,18 @@ exec(`${libExecutable} --file test/file/config.js -R test/file/reporter.js test/
         process.exit(1);
     }
 
-    console.log('running the test');
-
     const runnerEvents = stdout
         .toString()
         .trim()
         .split('\n')
         .map((evtName) => evtName.trim());
 
-    console.log(runnerEvents);
-
     assert.strictEqual(runnerEvents[0], 'start');
     assert.strictEqual(runnerEvents[1], 'suite'); // main process root suite
-    assert.strictEqual(runnerEvents[2], 'waiting'); // subprocess waiting event
+
+    // log from --file option
+    assert.strictEqual(runnerEvents[2], '>>>>> If we see this log then the file got loaded by the --file option');
+
     assert.strictEqual(runnerEvents[3], 'suite'); // subprocess root suite
     assert.strictEqual(runnerEvents[4], 'suite'); // subprocess top level suite
     assert(runnerEvents.includes('pass'), 'Test case was not executed');
