d7033b12b24af80882e7a6bcdb1a7f1212e689ef
saartochner <saart@lumigo.io>
2020-01-13 13:52:56 +0200

    symbol is not cloneable

--- src/bin/options/share-envs.ts
+++ src/bin/options/share-envs.ts
@@ -3,7 +3,7 @@ import { supportsWorkerThreads } from "../../thread";
 
 export default function applyShareEnvs(mocha: Mocha, shareEnvs?: boolean) {
   if (shareEnvs) {
-    if (supportsWorkerThreads()) {
+    if (!supportsWorkerThreads()) {
       throw new Error("The share-envs option is available only on node 12 and up.");
     }
     mocha.setEnvs(true);
