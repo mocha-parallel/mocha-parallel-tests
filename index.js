cae907268293b79e9ab8450b9d49de6547898f7f
saartochner <saart@lumigo.io>
2020-01-26 10:59:50 +0200

    wip

---
+++ index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+// This is here because https://stackoverflow.com/questions/50616253/how-to-resolve-chicken-egg-situation-with-tsc-and-npm-install
+require('./dist/bin/cli.js');
